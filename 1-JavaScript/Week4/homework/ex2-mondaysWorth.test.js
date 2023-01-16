15865f69e41dd3f35de3a3802fa292b148677ec3
Badar Us Salam <badaronline86@gmail.com>
2023-01-16 16:16:48 +0100

    JavaScript week4 Homework, 2nd Exercise finished

--- 1-JavaScript/Week4/homework/ex2-mondaysWorth.test.js
+++ 1-JavaScript/Week4/homework/ex2-mondaysWorth.test.js
@@ -31,8 +31,12 @@ const mondayTasks = [
 
 const hourlyRate = 25;
 
-function computeEarnings(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function computeEarnings(tasks, hourlyRate) {
+  const totalEarnings = tasks
+    .map((task) => task.duration)
+    .reduce((acc, duration) => acc + (duration / 60) * hourlyRate, 0)
+    .toFixed(2);
+  return `€${totalEarnings}`;
 }
 
 // ! Unit tests (using Jest)
