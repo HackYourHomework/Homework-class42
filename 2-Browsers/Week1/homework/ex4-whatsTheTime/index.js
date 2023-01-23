59de203a96708e99afe84a88fd660bbe8246412f
Daniil <daniil.kravchenko.92@yandex.ru>
2023-01-23 18:50:30 +0100

    delete commented lines in 4ex and 5ex

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -1,12 +1,5 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Browsers/Week1#exercise-4-whats-the-time
 
-1. Inside the `index.js`, complete the `addCurrentTime` to add the current time 
-  to the webpage. Make sure it's written in the HH:MM:SS notation (hour, minute,
-  second). Use `setInterval()` to make sure the time stays current.
-2. Have the function execute when it's loading in the browser.
-------------------------------------------------------------------------------*/
 const p = document.createElement('p');
 
 function addCurrentTime() {
@@ -16,8 +9,9 @@ function addCurrentTime() {
   p.textContent = time;
   document.querySelector('body').appendChild(p);
 
-}
+} 
 
 window.addEventListener('load', addCurrentTime);
-setInterval(addCurrentTime,600)
+setInterval(addCurrentTime, 600)
+
 
