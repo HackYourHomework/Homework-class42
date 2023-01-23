eb632bb47370676688cd6b52979a5c99f76feaac
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-01-23 13:10:02 +0100

    I finished the ex2 file task and run the test. At first it gave me an error because of the body font family. I added js file a new font family then the test passed. I tried to finish the ex1 file but stucked in assigning img src attributes. Ex1 file is not finished yet.

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -9,4 +9,20 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
 
-// TODO add your JavaScript code here.
+const newNickName = document.createElement('span');
+newNickName.textContent = 'Coder';
+document.getElementById('nickname').replaceWith(newNickName);
+const favFood = document.getElementById('fav-food');
+const newFavFood = document.createElement('span');
+newFavFood.textContent = 'Kebab';
+favFood.replaceWith(newFavFood);
+const hometown = document.getElementById('hometown');
+const newHometown = document.createElement('span');
+newHometown.textContent = 'Ankara';
+hometown.replaceWith(newHometown);
+
+const listItems = document.querySelectorAll('li');
+listItems.forEach((item) => item.classList.add('list-item'));
+
+//there is not a task like this but when I test the file it failed because of font-family and made me to add that code to js file, not in style.css!
+document.body.style.fontFamily = 'Arial, sans-serif';
