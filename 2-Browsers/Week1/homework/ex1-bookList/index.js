3a215e83daf5eb72daba1e04e9c5f073c80126ea
Yuliia Helievierova <juliageleverova@ziggo.nl>
2023-01-24 12:53:33 +0100

    style.css ex1

--- 2-Browsers/Week1/homework/ex1-bookList/index.js
+++ 2-Browsers/Week1/homework/ex1-bookList/index.js
@@ -18,7 +18,7 @@ https: //hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 //cspell: enable
 
 function createBookList(books) {
-  // TODO your code goes in here, return the ul element
+  
 }
 
 function main() {
