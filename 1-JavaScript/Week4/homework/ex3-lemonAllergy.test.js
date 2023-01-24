72613f7b64f2610e9b8d6e520961f4cd31836542
Yuliia Helievierova <117078871+YuliiaHelievierova@users.noreply.github.com>
2023-01-24 02:17:54 +0100

    Update ex3-lemonAllergy.test.js

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -28,27 +28,27 @@ const fruitBasket = [
 // ! Function under test
 function sanitizeFruitBasket(fruit, oddOneOut) {
   return fruit.filter((thing) => thing !== oddOneOut);
-  // TODO complete this function
+ 
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    // TODO replace next line with your code
+   
     expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
-    // Save the original contents of the fruit basket
+    
     const originalFruitBasketContents = [...fruitBasket];
     sanitizeFruitBasket(fruitBasket);
 
-    // TODO replace next line with your code
+   
     expect(originalFruitBasketContents).toEqual(fruitBasket);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
+   
     const result = sanitizeFruitBasket(fruitBasket, 'lemon');
     const newArray = [`apple`, `grapefruit`, `banana`, `watermelon`];
     expect(result).toEqual(newArray);
