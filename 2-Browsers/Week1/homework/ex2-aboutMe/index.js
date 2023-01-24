b66d466720047425b516b854284bc7c714486ad4
Yuliia Helievierova <juliageleverova@ziggo.nl>
2023-01-24 23:59:59 +0100

    ex2.5

--- 2-Browsers/Week1/homework/ex2-aboutMe/index.js
+++ 2-Browsers/Week1/homework/ex2-aboutMe/index.js
@@ -9,4 +9,10 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 3. Look in the css file!
 ------------------------------------------------------------------------------*/
 
-// TODO add your JavaScript code here.
+document.getElementById('nickname').textContent = 'Julia';
+document.getElementById('fav-food').textContent = 'Shawarma';
+document.getElementById('hometown').textContent = 'Chernihiv';
+
+document.body.style.fontFamily = 'Arial, sans-serif';
+const listItem = document.querySelectorAll('li');
+listItem.forEach((li) => (li.classList = li.classList = 'list-item'));
