8b7ed1ee7d7680b8b4a32018307b91e2771a3375
Daniil <daniil.kravchenko.92@yandex.ru>
2023-01-31 19:09:06 +0100

    i hate this cate

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -1,27 +1,30 @@
 'use strict';
 
 const image = document.querySelector('img');
-  image.style.left = '0px';
-  let distanceOfCat = 0;
-  const maxDistanceOfCat = 1660;
+image.style.left = '0px';
+let distance = 0;
+const speed = 10;
+window.addEventListener('load', catWalk);
+let interval;
 
-  function changeCatImage(){
-   if(image.src === 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif'){
-      image.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif'
-   } else if (image.src === 'http://www.anniemation.com/clip_art/images/cat-walk.gif'){
-      image.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif'
-   }}
 
 function catWalk() {
-  distanceOfCat += 10;
-  image.style.left = `${distanceOfCat}px`;
-  if(image.style.left === `${maxDistanceOfCat}px`){
-   distanceOfCat = 0;
-  } 
-  if (image.style.left === `${maxDistanceOfCat / 2}px`){
-   changeCatImage() // I don't understand how to stop the function for 5 seconds
-  }
+   distance = distance + speed;
+   image.style.left = `${distance}px`
+   if(distance > window.innerWidth / 2){
+      clearInterval(interval);
+      image.src =
+        'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+      setTimeout(() => {
+        image.src =
+          'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+        interval = setInterval(catWalk, 50);
+      }, 5000);
+    }
+    if (distance > window.innerWidth) {
+      image.style.left = '0px';
+      return;
+    }
+    image.style.left = `${distance + speed}px`;
 }
-
-window.addEventListener('load', catWalk)
-setInterval(catWalk, 50);
+setInterval(catWalk, 50)
