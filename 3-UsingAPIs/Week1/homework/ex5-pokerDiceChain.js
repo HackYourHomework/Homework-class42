cc17c7eb543a9e50037e99fd61540ee2e242ed15
Mohammed Aleryni <mym.aleryani@gmail.com>
2023-02-12 01:24:27 +0100

    I  expanded the given promise chain to include five dice

--- 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
+++ 3-UsingAPIs/Week1/homework/ex5-pokerDiceChain.js
@@ -25,8 +25,20 @@ function rollDice() {
     })
     .then((value) => {
       results.push(value);
+      return rollDie(3);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDie(4);
+    })
+    .then((value) => {
+      results.push(value);
+      return rollDie(5);
+    })
+    .then((value) => {
+      results.push(value);
       return results;
-    });
+    })
 }
 
 function main() {
