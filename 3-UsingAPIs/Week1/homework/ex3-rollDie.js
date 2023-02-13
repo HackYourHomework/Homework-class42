b8a6c612ed5835c297df85811f22ed32c1e13f22
Mohammed Aleryni <mym.aleryani@gmail.com>
2023-02-13 16:42:39 +0100

    I modified the code so now when Die rolled out the table the code would stop and I added the explanation to the bottom of the code.

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -11,49 +11,49 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-Usin
   explanation? Add your answer as a comment to be bottom of the file.
 ------------------------------------------------------------------------------*/
 
-const rollDie=()=>{
-  return new Promise((resolve,reject)=>{
-    
-      // Compute a random number of rolls (3-10) that the die MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
+const rollDie = () => {
+  return new Promise((resolve, reject) => {
+    // Compute a random number of rolls (3-10) that the die MUST complete
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
 
-  const rollOnce = (roll) => {
-    // Compute a random die value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die value is now: ${value}`);
+    const rollOnce = (roll) => {
+      // Compute a random die value for the current roll
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Die value is now: ${value}`);
 
-    // Use callback to notify that the die rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      reject(new Error('Oops... Die rolled off the table.'));
-    }
+      // Use callback to notify that the die rolled off the table after 6 rolls
+      if (roll > 6) {
+        // TODO replace "error" callback
+         return reject(new Error('Oops... Die rolled off the table.'));
+      }
 
-    // Use callback to communicate the final die value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      resolve(value)
-    }
+      // Use callback to communicate the final die value once finished rolling
+      if (roll === randomRollsToDo) {
+        // TODO replace "success" callback
+         resolve(value);
+      }
 
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
+      // Schedule the next roll todo until no more rolls to do
+      if (roll < randomRollsToDo) {
+        setTimeout(() => rollOnce(roll + 1), 500);
+      }
+    };
 
-  // Start the initial roll
-  rollOnce(1);
-    
-  })
-}
+    // Start the initial roll
+    rollOnce(1);
+  });
+};
 
 function main() {
   // TODO Refactor to use promise
-  rollDie().then((value)=>{
-    console.log(`Success! Die settled on ${value}.`);
-  }).catch((error)=>{
-    console.log(error.message);
-  })
+  rollDie()
+    .then((value) => {
+      console.log(`Success! Die settled on ${value}.`);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 }
 
 // ! Do not change or remove the code below
@@ -62,6 +62,4 @@ if (process.env.NODE_ENV !== 'test') {
 }
 module.exports = rollDie;
 
-
-
-// After I refactored the code to use promises the problem still occurs but not the exact same. The reject value was set at first and not updated, That's because the resolve and reject functions must be called once when the state changed and any further calls would be ignored.
+//when I added a return statement to a rejected promise, the rejection will be propagated to the caller as the value returned by the function and the function will break. If I didn't add a return statement to a rejected promise, the rejection will still occur, but the error will be thrown to catch and the function will continue.
