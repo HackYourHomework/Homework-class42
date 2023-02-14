3d5e160f7bb4ae9c2a8c5c1c27ba232fcd825ebb
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-02-14 19:35:30 +0100

    API week1 ex4 done

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -27,9 +27,10 @@ exercise file.
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
-  // TODO Refactor this function
+
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  return Promise.all(dice.map(rollDie));
+
 }
 
 function main() {
