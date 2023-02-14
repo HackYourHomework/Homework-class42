d2eb2324e376b6d1255b28eec190d09b6f1829ff
Abdulrahman <abdogohjal@gmail.com>
2023-02-14 21:12:08 +0100

    done

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -49,3 +49,29 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   let interval = window.setInterval(catWalk, 50);
 });
+
+// const image = document.querySelector('img');
+// image.style.left = '0px';
+// let distanceOfCat = 0;
+// const catSpeed = 10;
+// const maxDistanceOfCat = window.innerWidth;
+// let interval = setInterval(catWalk, 50);
+// window.addEventListener('load', catWalk);
+
+// function catWalk() {
+//   if (image.style.left === `${maxDistanceOfCat} + px`) {
+//     distanceOfCat = 0;
+//   } else if (image.style.left >= `${maxDistanceOfCat / 2} + px`) {
+//     image.style.left = `${maxDistanceOfCat / 2 + 10} `;
+//     clearInterval(interval);
+//     image.src =
+//       'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+//     setTimeout(() => {
+//       image.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+//       interval = setInterval(catWalk, 50);
+//     }, 5000);
+//   } else {
+//     distanceOfCat += catSpeed;
+//     image.style.left = `${distanceOfCat}px`;
+//   }
+// }
