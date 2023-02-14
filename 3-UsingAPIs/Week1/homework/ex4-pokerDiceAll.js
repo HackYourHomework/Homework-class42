2589935700198d54228edabb1ad969dbb5f888d2
Abdulrahman <abdogohjal@gmail.com>
2023-02-14 22:26:32 +0100

    done

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -27,9 +27,9 @@ exercise file.
 const rollDie = require('../../helpers/pokerDiceRoller');
 
 function rollDice() {
-  // TODO Refactor this function
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  const diceRes = dice.map((die) => rollDie(die));
+  return Promise.all(diceRes);
 }
 
 function main() {
