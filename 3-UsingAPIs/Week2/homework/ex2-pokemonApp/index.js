b73cc6ed623055027aa86dcb9d3f4bca5fd52daf
Daniil <daniil.kravchenko.92@yandex.ru>
2023-02-16 00:01:20 +0100

    not complete version of ex2

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -22,18 +22,56 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
+async function fetchData(url) {
+  // TODO complete this function
+  const res = await fetch(url)
+  const jsonData = await res.json()
+  if(res.ok){
+    console.log(jsonData)
+    return jsonData
+  } else {
+    throw new Error('HTTP error')
+  }
+
+}
+
+function fetchAndPopulatePokemons(data) {
+  // TODO complete this function
+
+  const body = document.querySelector('body')
+  const button = document.createElement('button')
+  button.setAttribute('type','button')
+  button.textContent = 'Get Pokemon!'
+  button.className = 'grid button'
+  const select = document.createElement('select')
+  
+  select.className = 'grid'
+
+  body.appendChild(button)
+  body.appendChild(select)
+
+  for(const pokemon of data.results){
+    const option = document.createElement('option');
+    option.textContent = pokemon.name
+    option.setAttribute('value',pokemon.name)
+
+  button.addEventListener('click', () => {
+          select.appendChild(option)
+        }
+        )
+      }
+}
+
+function fetchImage(data) {
   // TODO complete this function
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
+async function main() {
   // TODO complete this function
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=151'
+  const data = await fetchData(url)
+  fetchAndPopulatePokemons(data)
+  fetchImage(data)
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
-}
-
-function main() {
-  // TODO complete this function
-}
+window.addEventListener('load', main)
