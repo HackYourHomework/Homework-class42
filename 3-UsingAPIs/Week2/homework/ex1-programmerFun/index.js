b17135f9ee9395d12a469b6818d54b0f8274b475
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-02-18 10:52:41 +0100

    finished the ex1 file and passed the test

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -18,28 +18,45 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
 function requestData(url) {
-  // TODO return a promise using `fetch()`
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error('HTTP or Network problem!');
+    }
+    return response.json();
+  });
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
+  const imgElement = document.createElement('img');
+  imgElement.src = data.img;
+  document.body.appendChild(imgElement);
   console.log(data);
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
+  const errorElement = document.createElement('h1');
+  errorElement.textContent = error;
+  document.body.appendChild(errorElement);
   console.log(error);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
+async function main() {
+  try {
+    return await requestData('https://xkcd.now.sh/?comic=latest').then(
+      (data) => {
+        renderImage(data);
+      }
+    );
+  } catch (error) {
+    renderError(error);
+  }
+  /* requestData('https://xkcd.now.sh/?comic=latest')
     .then((data) => {
       renderImage(data);
     })
     .catch((error) => {
       renderError(error);
-    });
+    }); */
 }
 
 window.addEventListener('load', main);
