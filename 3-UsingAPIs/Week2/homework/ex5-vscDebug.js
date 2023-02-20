d08225a3d15ad0c27035304b9d497c1e2f647656
Mohammed Aleryni <mym.aleryani@gmail.com>
2023-02-21 00:47:31 +0100

    init version

--- 3-UsingAPIs/Week2/homework/ex5-vscDebug.js
+++ 3-UsingAPIs/Week2/homework/ex5-vscDebug.js
@@ -8,22 +8,24 @@ const fetch = require('node-fetch');
 
 async function getData(url) {
   const response = await fetch(url);
-  return response.json();
+  return  await response.json();
 }
 
 function renderLaureate({ knownName, birth, death }) {
   console.log(`\nName: ${knownName.en}`);
-  console.log(`Birth: ${birth.date}, ${birth.place.locationString}`);
-  console.log(`Death: ${death.date}, ${death.place.locationString}`);
+  console.log(`Birth: ${birth.date}, ${birth.place.locationString.en}`);
+  if (death){
+    console.log(`Death: ${death.date}, ${death.place.locationString.en}`);
+  }
 }
 
 function renderLaureates(laureates) {
-  laureates.forEach(renderLaureate);
+  laureates['laureates'].forEach(renderLaureate);
 }
 
 async function fetchAndRender() {
   try {
-    const laureates = getData(
+    const laureates =await getData(
       'http://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
     );
     renderLaureates(laureates);
