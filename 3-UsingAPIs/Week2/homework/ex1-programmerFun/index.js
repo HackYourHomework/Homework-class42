bf80b1cf8fbc4ae00df909fc742915534409c40d
Abdulrahman <abdogohjal@gmail.com>
2023-02-20 18:01:31 +0100

    deleted code and fixed it and run test runner on ex1 and 2 and 3

--- 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/homework/ex1-programmerFun/index.js
@@ -17,29 +17,39 @@ Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-Usin
    url with `.shx`. There is no server at the modified url, therefore this 
    should result in a network (DNS) error.
 ------------------------------------------------------------------------------*/
-function requestData(url) {
-  // TODO return a promise using `fetch()`
+async function requestData(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP ${res.status} ${res.statusText}`);
+    }
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function renderImage(data) {
-  // TODO render the image to the DOM
-  console.log(data);
+  const image = document.createElement('img');
+  image.src = String(data.img);
+  image.alt = 'data.img';
+  document.body.appendChild(image);
+  if (image.src.length === 0) throw new Error(':( No image found');
 }
 
 function renderError(error) {
-  // TODO render the error to the DOM
-  console.log(error);
+  const h1 = document.createElement('h1');
+  h1.textContent = error.message;
+  document.body.appendChild(h1);
 }
 
-// TODO refactor with async/await and try/catch
-function main() {
-  requestData('https://xkcd.now.sh/?comic=latest')
-    .then((data) => {
-      renderImage(data);
-    })
-    .catch((error) => {
-      renderError(error);
-    });
+async function main() {
+  try {
+    const data = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(data);
+  } catch (error) {
+    renderError(error);
+  }
 }
 
 window.addEventListener('load', main);
--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -22,18 +22,63 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchData(url) {
+  try {
+    const data = await fetch(url).then((response) => response.json());
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function fetchAndPopulatePokemons(data) {
+  const select = document.querySelector('select');
+  select.innerHTML = '';
+  const pokemons = data.results;
+  pokemons.forEach((pokemon) => {
+    const option = document.createElement('option');
+    option.value = pokemon.url;
+    option.textContent = pokemon.name;
+    select.appendChild(option);
+  });
+  select.addEventListener('change', (event) => {
+    fetchImage(event);
+  });
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(event) {
+  try {
+    const selectUrl = event.target.value;
+    const img = document.querySelector('img');
+    const imgData = await fetchData(selectUrl);
+    img.alt = 'Pokemon';
+    img.src =
+      imgData.sprites['versions']['generation-v']['black-white']['animated'][
+        'front_default'
+      ];
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 function main() {
-  // TODO complete this function
+  document.body.innerHTML = `
+      <h2><b>Pokemons</b></h2>
+      <button id="button">Get Pokemons</button>
+      <select id="select">
+          <option value="" disabled selected>Press [Get Pokemons] first</option>
+      </select>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Pokemon">
+  `;
+  const button = document.querySelector('button');
+  const select = document.querySelector('select');
+  button.addEventListener('click', async () => {
+    const data = await fetchData('https://pokeapi.co/api/v2/pokemon?limit=151');
+    fetchAndPopulatePokemons(data);
+  });
+  select.addEventListener('change', (event) => {
+    fetchImage(event.target.value);
+  });
 }
+
+window.addEventListener('load', main);
