20b9089ef4d01eb4c8512c75f236ae50bd782a0e
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-02-21 00:26:55 +0100

    API week2 ex6 done

--- 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
+++ 3-UsingAPIs/Week2/homework/ex6-browserDebug/index.js
@@ -6,7 +6,8 @@ Full description at:https://github.com/HackYourFuture/Homework/blob/main/3-Using
 
 async function getData(url) {
   const response = await fetch(url);
-  return response.json();
+  const data = response.json();
+  return data;
 }
 
 function createAndAppend(name, parent, options = {}) {
@@ -32,10 +33,12 @@ function renderLaureate(ul, { knownName, birth, death }) {
   const li = createAndAppend('li', ul);
   const table = createAndAppend('table', li);
   addTableRow(table, 'Name', knownName.en);
-  addTableRow(table, 'Birth', `${birth.date}, ${birth.place.locationString}`);
-  addTableRow(table, 'Death', `${death.date}, ${death.place.locationString}`);
+  addTableRow(table, 'Birth', `${birth.date}, ${birth.place.locationString.en}`);
+  if(death){
+    addTableRow(table, 'Death', `${death.date}, ${death.place.locationString.en}`);
+  }
 }
-
+  
 function renderLaureates(laureates) {
   const ul = createAndAppend('ul', document.body);
   laureates.forEach((laureate) => renderLaureate(ul, laureate));
@@ -43,12 +46,13 @@ function renderLaureates(laureates) {
 
 async function fetchAndRender() {
   try {
-    const laureates = getData(
+    const {laureates} = await getData(
       'https://api.nobelprize.org/2.0/laureates?birthCountry=Netherlands&format=json&csvLang=en'
     );
     renderLaureates(laureates);
   } catch (err) {
     console.error(`Something went wrong: ${err.message}`);
+    console.error(err.stack)
   }
 }
 
