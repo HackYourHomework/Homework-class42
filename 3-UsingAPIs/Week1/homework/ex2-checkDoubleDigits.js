73c8e7f0e542c885ca2ab6eafca9c157583cc6fc
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-02-25 12:33:00 +0100

    fixed error message

--- 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
+++ 3-UsingAPIs/Week1/homework/ex2-checkDoubleDigits.js
@@ -31,19 +31,19 @@ return promise;
 function main() {
   checkDoubleDigits(9) // should reject
     .then((message) => console.log(message))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error.message));
 
   checkDoubleDigits(10) // should resolve
     .then((message) => console.log(message))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error.message));
 
   checkDoubleDigits(99) // should resolve
     .then((message) => console.log(message))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error.message));
 
   checkDoubleDigits(100) // should reject
     .then((message) => console.log(message))
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error.message));
 }
 
 // ! Do not change or remove the code below
